3425cdb1fbb8d5ea1a3cedde2a12008a4b67eb71
TusharSatija <tusharsatija737@gmail.com>
2024-07-20 20:16:01 +0530

    backend practice

---
+++ lecture-8/app.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+app.set('view engine','ejs');
+app.set(path.join(__dirname,'views'));
+
+
+app.get('/home',(req,res)=>{
+    res.render('index');
+    // res.send('this is a home page .');
+})
+
+app.post('/submit',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+})
+
+app.get('/about',(req,res)=>{
+    res.send('this is a about page');
+})
+
+app.get('/products',(req,res)=>{
+    res.send('this is a product page');
+})
+
+
+
+app.listen(3333,()=>{
+    console.log("server is running at port 3333");
+})
